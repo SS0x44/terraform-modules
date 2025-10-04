93c2705cdf1fc6ef3611e43553943b698a163bf7
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-04 15:32:37 +0530

    Update main.tf

--- v1.13.3/aws-modules/ec2/main.tf
+++ v1.13.3/aws-modules/ec2/main.tf
@@ -1,13 +1,15 @@
-# Security Group
+#------------------------------
+# 🔐 Security Group
+#------------------------------
 resource "aws_security_group" "gitlab_runner_sg" {
-  name        = var.runner_sg
-  description = "Security group for Gitlab runner EC2 instances"
-  vpc_id      = data.aws_vpc.default.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  name           = var.runner_sg
+  description    = "Security group for Gitlab runner EC2 instances"
+  vpc_id         = data.aws_vpc.select_exisitng_vpc.id
+  ingress {      
+    from_port    = 22
+    to_port      = 22
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
@@ -15,15 +17,16 @@ resource "aws_security_group" "gitlab_runner_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    Name = var.runner_sg
+  tags          = {
+    Name        = var.runner_sg
   }
 }
-
+#------------------------------
 # 🚀 Launch Template
+#------------------------------
 resource "aws_launch_template" "gitlab_runner_launch_template" {
- depends_on                     = [aws_security_group.gitlab_runner_sg, data.aws_ami.gitlab_runner_ami]
-  image_id                      = data.aws_ami.latest_golden_ami.id
+  depends_on                    = [aws_security_group.gitlab_runner_sg, data.aws_ami.gitlab_runner_ami]
+  image_id                      = data.aws_ami.gitlab_runner_ami.id
   instance_type                 = var.instance_type
   update_default_version        = true
   user_data                     = base64encode(file("${path.module}/scripts/required_packages.sh"))
@@ -32,36 +35,49 @@ resource "aws_launch_template" "gitlab_runner_launch_template" {
     security_groups             = [aws_security_group.gitlab_runner_sg.id]
   }
   tag_specifications {
-    resource_type               = "instance"
-    tags                        = {
-      Name                      = var.gl_runner_name
-      Environment               = var.env_short
+    resource_type              = "instance"
+    tags                       = {
+      Name                     = var.gl_runner_name
+      Environment              = var.env_short
     }
   }
   lifecycle {
     create_before_destroy      = true
   }
 }
-
+#------------------------------
 # 📈 Auto Scaling Group
+#------------------------------
 resource "aws_autoscaling_group" "gitlab_runner_asg" {
-  name                      = var.asg_group_name
-  max_size                  = var.max_size
-  min_size                  = var.min_size
-  desired_capacity          = var.desired_capacity
-  vpc_zone_identifier       = data.aws_subnets.select_exisitng_vpc_subnets.ids
+  depends_on            = [aws_launch_template.gitlab_runner_launch_template]
+  name                  = var.asg_group_name
+  max_size              = var.max_size
+  min_size              = var.min_size
+  desired_capacity      = var.desired_capacity
+  vpc_zone_identifier   = data.aws_subnets.select_exisitng_vpc_subnets.ids
   launch_template {
-    id                     = aws_launch_template.gitlab_runner_launch_template.id
-    version                = "$Latest"
+    id                  = aws_launch_template.gitlab_runner_launch_template.id
+    version             = "$Latest"
   }
   tag {
-    key                    = "Name"
-    value                  = "gitlab-runner-instance"
-    propagate_at_launch    = true
+    key                 = "Name"
+    value               = "gitlab-runner-instance"
+    propagate_at_launch = true
   }
   lifecycle {
     create_before_destroy = true
+ }
+}
+#------------------------------
+# 🔄 Instance Refresh
+#------------------------------
+resource "aws_autoscaling_group_instance_refresh" "gitlab_runner_refresh" {
+  depends_on               = [aws_autoscaling_group.gitlab_runner_asg]
+  autoscaling_group_name   = aws_autoscaling_group.gitlab_runner_asg.name
+  strategy                 = "Rolling"
+  triggers                 = ["launch_template"]
+  preferences {
+    min_healthy_percentage = 90
+    instance_warmup        = 300
   }
 }
-
-
