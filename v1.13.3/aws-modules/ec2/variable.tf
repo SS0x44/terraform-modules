9317969b0561bd3a38d3edad9a287c0d672fdf14
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-04 14:49:41 +0530

    Update variable.tf

--- v1.13.3/aws-modules/ec2/variable.tf
+++ v1.13.3/aws-modules/ec2/variable.tf
@@ -18,3 +18,30 @@ variable "env_tags" {
   type        = map(string)
   description = "Environment-specific tags"
 }
+
+variable "ami_name" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in ASG"
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in ASG"
+}
+
+variable "desired_capacity" {
+  description = "Desired number of instances in ASG"
+}
+
