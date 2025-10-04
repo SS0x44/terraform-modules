6eedf7ecdd9af87fb75c7fb2d1f42e78695da3f3
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-04 15:16:54 +0530

    Update variable.tf

--- v1.13.3/aws-modules/ec2/variable.tf
+++ v1.13.3/aws-modules/ec2/variable.tf
@@ -3,45 +3,45 @@ variable "account_id" {
   type        = string
   description = "AWS account ID"
 }
-
 variable "region" {
   type        = string
   description = "AWS region"
 }
-
 variable "namespace" {
   type        = string
   description = "Project namespace"
 }
-
 variable "env_tags" {
   type        = map(string)
   description = "Environment-specific tags"
 }
-
 variable "ami_name" {
   description = "AMI ID for EC2 instances"
   type        = string
 }
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instances"
-  type        = string
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
 }
-
 variable "min_size" {
   description = "Minimum number of instances in ASG"
 }
-
 variable "max_size" {
   description = "Maximum number of instances in ASG"
 }
-
 variable "desired_capacity" {
   description = "Desired number of instances in ASG"
 }
+variable "asg_group_name" {
+  description = "Name of the Auto Scaling Group"
+  type        = string
+}
+variable "runner_sg" {
+  description = "Name of the security group for GitLab Runner"
+  type        = string
+}
+variable "gl_runner_name" {
+  description = "Tag name for GitLab Runner EC2 instance"
+  type        = string
+}
+
 
