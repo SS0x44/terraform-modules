ca85edca3d7154eb896e1f25459d5df2e71814f8
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-04 14:47:23 +0530

    Update data.tf

--- v1.13.3/aws-modules/ec2/data.tf
+++ v1.13.3/aws-modules/ec2/data.tf
@@ -1,1 +1,27 @@
 
+data "aws_vpc" "select_exisitng_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["<placeholder-company-enterprise-vpc>"]
+  }
+}
+
+data "aws_subnets" "select_exisitng_vpc_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.select_exisitng_vpc.id]
+  }
+}
+
+data "aws_ami" "latest_golden_ami" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["<placeholder-company-golden-ami-pattern-*>"] 
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["<placeholder_for_aws_ami_account_id>"] 
+}
