d05fb3f63cc72cb2eaa60eef5c41ce4b079ac55d
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-10 22:56:50 +0530

    Update variable.tf

--- v1.13.3/aws-modules/event-bridge/variable.tf
+++ v1.13.3/aws-modules/event-bridge/variable.tf
@@ -1,3 +1,27 @@
+# variables.tf
+variable "aws_account_id" {
+  type        = string
+  description = "AWS account ID"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "deploy_color" {
+  type        = string
+  description = "Project namespace"
+}
+
+variable "env_tags" {
+  type        = map(string)
+}
+
+variable "tags" {
+  type        = map(string)
+}
+
 variable "lambda_sqs_mappings" {
   type = map(object({
     function_arn = string
