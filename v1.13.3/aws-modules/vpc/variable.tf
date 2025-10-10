46525ff1fc48c96168b316cf526f3954845d3cac
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-10 18:01:46 +0530

    Update variable.tf

--- v1.13.3/aws-modules/vpc/variable.tf
+++ v1.13.3/aws-modules/vpc/variable.tf
@@ -1,5 +1,19 @@
-variable "region" { type = string }
-variable "vpc_cidr" { type = string }
-variable "public_subnet_cidr" { type = string }
-variable "private_subnet_cidr" { type = string }
-variable "az" { type = string }
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = list(string)
+}
+
+variable "private_subnet_cidr" {
+  type = list(string)
+}
+
+variable "az" {
+  type = list(string)
+}
