ce9ca0e2f10a0ee9c12d1518ab1c7efcd28eff7e
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-10 22:30:20 +0530

    Update main.tf

--- v1.13.3/aws-modules/event-bridge/main.tf
+++ v1.13.3/aws-modules/event-bridge/main.tf
@@ -1,8 +1,13 @@
 resource "aws_lambda_event_source_mapping" "event_sqs_trigger" {
-  for_each = var.lambda_sqs_mappings
-  event_source_arn                      = each.value.queue_arn
-  function_name                         = each.value.function_arn
-  enabled                               = true
-  batch_size                            = 10
-  maximum_batching_window_in_seconds   = 5
+  for_each = {
+    for name in keys(aws_sqs_queue.queues) : name => {
+      queue_arn    = aws_sqs_queue.queues[name].arn
+      function_arn = aws_lambda_function.functions[name].arn
+    }
+  }
+  event_source_arn                    = each.value.queue_arn
+  function_name                       = each.value.function_arn
+  enabled                             = true
+  batch_size                          = 10
+  maximum_batching_window_in_seconds = 5
 }
