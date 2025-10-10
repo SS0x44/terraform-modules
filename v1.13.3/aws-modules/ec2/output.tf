e24b6d99de526cff6337e5a2bd8d2fbeeebcc1c8
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-10 11:25:08 +0530

    Update output.tf

--- v1.13.3/aws-modules/ec2/output.tf
+++ v1.13.3/aws-modules/ec2/output.tf
@@ -1,14 +1,10 @@
 output "security_group_id" {
-  description = "ID of the EC2 security group"
   value       = aws_security_group.ec2_sg.id
 }
-
 output "launch_template_id" {
-  description = "ID of the EC2 launch template"
-  value       = aws_launch_template.ec2_template.id
+  value       = aws_launch_template.launch_template.id
+}
+output "autoscaling_group_name" {
+  value       = aws_autoscaling_group.ec2_asg_fleet.name
 }
 
-output "autoscaling_group_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.ec2_asg.name
-}
