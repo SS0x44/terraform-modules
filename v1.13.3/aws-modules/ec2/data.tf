567a4eb2eca7e91094e3edcb79f038d0b8538168
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-10 11:19:21 +0530

    Update data.tf

--- v1.13.3/aws-modules/ec2/data.tf
+++ v1.13.3/aws-modules/ec2/data.tf
@@ -1,31 +1,37 @@
-
-data "aws_vpc" "select_exisitng_vpc" {
+# Data Source 01 : VPC 
+----------------------------------------------------
+data "aws_vpc" "select_existing_vpc" {
   filter {
     name   = "tag:Name"
     values = [var.vpc_name_tag]
   }
   filter {
     name   = "tag:Environment"
-    values = [var.vpc_environmet_tag]
-  }  
-}
-
-data "aws_subnets" "select_exisitng_vpc_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.select_exisitng_vpc.id]
+    values = [var.vpc_environment_tag]
   }
 }
 
+# Data Source 02 : VPC Subnets 
+------------------------------------------------------
+data "aws_subnets" "select_existing_vpc_subnets" {
+  depends_on = [data.aws_vpc.select_existing_vpc]
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.select_existing_vpc.id]
+  }
+}
+
+# Data Source 03 : AMI 
+-----------------------------------------------------
 data "aws_ami" "latest_golden_ami" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["<placeholder-company-golden-ami-pattern-*>"] 
+    values = ["<placeholder-company-golden-ami-pattern-*>"]
   }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["<placeholder_for_aws_ami_account_id>"] 
+  owners = ["<placeholder_for_aws_ami_account_id>"]
 }
