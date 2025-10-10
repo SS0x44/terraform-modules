8f26c71f283c6cf81bb24bd0c5b521b0d266f223
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-10 18:10:41 +0530

    Update main.tf

--- v1.13.3/aws-modules/vpc/main.tf
+++ v1.13.3/aws-modules/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "main-vpc" }
+  tags                 =  merge(var.tags,{ Name = var.vpc_name })
 }
 
 resource "aws_subnet" "public" {
@@ -11,33 +11,33 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
   availability_zone       = var.az
-  tags = { Name = "public-subnet" }
+  tags                    =  merge(var.tags,{  Name = var.public_subnet_name })
 }
 
 resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = var.az
-  tags = { Name = "private-subnet" }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.az
+  tags                    =  merge(var.tags,{  Name = var.private_subnet_name })
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags   = { Name = "main-igw" }
+  tags   = { Name = var.igw_name }
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  tags   = { Name = "public-rt" }
+resource "aws_route_table" "public_route" {
+  vpc_id                  = aws_vpc.main.id
+  tags                    =  merge(var.tags,{  Name = var.public_route_name })
 }
 
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
+  route_table_id         = aws_route_table.public_route.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-resource "aws_route_table_association" "public_assoc" {
+resource "aws_route_table_association" "public_route_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
@@ -70,5 +70,6 @@ resource "aws_security_group" "public_sg" {
 
   tags = { Name = "public_sg" }
 }
+
 
 
