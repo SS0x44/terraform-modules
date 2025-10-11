04935e86e9ab52e2a7b08afd43ab164ee4734aa1
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 14:07:26 +0530

    Update data.tf

--- v1.13.3/aws-modules/lambda/data.tf
+++ v1.13.3/aws-modules/lambda/data.tf
@@ -1,6 +1,1 @@
-data "archive_file" "lambda_zip_archive" {
-  for_each    = { for i, name in var.function_names : i => name }
-  type        = "zip"
-  source_dir  = "${path.module}/src/${each.value}"
-  output_path = "${path.module}/build/${each.value}.zip"
-}
+
