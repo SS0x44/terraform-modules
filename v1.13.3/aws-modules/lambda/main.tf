13325162b65b4e5e68dba1a4d6e15bf9a9071a00
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 11:25:05 +0530

    Update main.tf

--- v1.13.3/aws-modules/lambda/main.tf
+++ v1.13.3/aws-modules/lambda/main.tf
@@ -1,19 +1,22 @@
 resource "aws_lambda_function" "lambda_functions" {
-  depends_on       = [aws_iam_role_policy.lambda_s3_access]
-  for_each         = { for name in var.function_names : name => name }
+  depends_on = [aws_iam_role_policy.lambda_s3_access]
+  for_each = { for i, name in var.function_names : name => i }
+  
   function_name    = each.key
   role             = aws_iam_role.lambda_exec.arn
-  handler          = var.handlers[each.key]
-  runtime          = var.runtimes[each.key]
-  filename         = data.archive_file.lambda_zip_archive[each.key].output_path
-  source_code_hash = data.archive_file.lambda_zip_archive[each.key].output_base64sha256
-  timeout          = var.timeout[each.key]
-  memory_size      = var.memory_size[each.key]
+  handler          = var.handlers[each.value]
+  runtime          = var.runtimes[each.value]
+  filename         = data.archive_file.lambda_zip_archive[each.value].output_path
+  source_code_hash = data.archive_file.lambda_zip_archive[each.value].output_base64sha256
+  timeout          = var.timeout[each.value]
+  memory_size      = var.memory_size[each.value]
   tags             = var.tags
+
   environment {
-    variables = var.environment_variables_map[each.key]
+    variables = var.environment_variables_list[each.value]
   }
   lifecycle {
     ignore_changes = [source_code_hash]
   }
 }
+
