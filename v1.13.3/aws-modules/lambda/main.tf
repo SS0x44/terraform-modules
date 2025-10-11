82e36951c6dd6b53ea8b4523a3773c5754447926
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 11:15:33 +0530

    Update main.tf

--- v1.13.3/aws-modules/lambda/main.tf
+++ v1.13.3/aws-modules/lambda/main.tf
@@ -1,20 +1,19 @@
 resource "aws_lambda_function" "lambda_functions" {
-  depends_on       = [aws_iam_role_policy.lambda_s3_access] 
-  function_name    = var.function_name
+  depends_on       = [aws_iam_role_policy.lambda_s3_access]
+  for_each         = { for name in var.function_names : name => name }
+  function_name    = each.key
   role             = aws_iam_role.lambda_exec.arn
-  handler          = var.handler
-  runtime          = var.runtime
-  filename         = data.archive_file.lambda_zip_archive.output_path
-  source_code_hash = data.archive_file.lambda_zip_archive.output_base64sha256
-  timeout          = var.timeout                     
-  memory_size      = var.memory_size                
-  publish          = true                            
-  package_type     = "Zip"  
-  tags             = var.tags    
+  handler          = var.handlers[each.key]
+  runtime          = var.runtimes[each.key]
+  filename         = data.archive_file.lambda_zip_archive[each.key].output_path
+  source_code_hash = data.archive_file.lambda_zip_archive[each.key].output_base64sha256
+  timeout          = var.timeout[each.key]
+  memory_size      = var.memory_size[each.key]
+  tags             = var.tags
   environment {
-    variables = var.environment_variables            
-  }                         
+    variables = var.environment_variables_map[each.key]
+  }
   lifecycle {
-    ignore_changes = [source_code_hash]              
+    ignore_changes = [source_code_hash]
   }
 }
