e86c177ff15a98dc06a8e2a9efdeefbda1fa8dc2
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 16:44:36 +0530

    Update variable.tf

--- v1.13.3/aws-modules/s3/variable.tf
+++ v1.13.3/aws-modules/s3/variable.tf
@@ -1,32 +1,9 @@
 # variables.tf
-variable "aws_account_id" {
-  type        = string
-  description = "AWS account ID"
-}
-
-variable "region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "env_tags" {
-  type        = map(string)
-  description = "Environment-specific tags"
-}
 variable "bucket_name" {
   type = string
 }
 
-variable "log_bucket_name" {
-  type = string
-}
-
 variable "enable_versioning" {
-  type    = bool
-  default = true
-}
-
-variable "enable_logging" {
   type    = bool
   default = true
 }
