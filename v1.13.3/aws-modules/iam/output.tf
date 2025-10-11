e0aeca28b7967840a1e30fc0925b50fc5ff2b1c9
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 18:08:39 +0530

    Update output.tf

--- v1.13.3/aws-modules/iam/output.tf
+++ v1.13.3/aws-modules/iam/output.tf
@@ -1,3 +1,7 @@
 output "iam_role_names" {
   value = { for name in var.iam_role_names : name => aws_iam_role.roles[name].name }
 }
+
+output "iam_role_arns" {
+  value = { for name in var.iam_role_names : name => aws_iam_role.roles[name].arn }
+}
