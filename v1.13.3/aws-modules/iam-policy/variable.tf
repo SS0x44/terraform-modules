349d26975389c4d7252efcdf58306cabfe1e3fce
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 17:45:18 +0530

    Update variable.tf

--- v1.13.3/aws-modules/iam-policy/variable.tf
+++ v1.13.3/aws-modules/iam-policy/variable.tf
@@ -1,1 +1,25 @@
+"sid" { 
+type        = list(string) 
+} 
+"iam_policy_names" { 
+type        = list(string) 
+} 
+variable "tags" { 
+type        = map(string) 
+}
+variable "principal" { 
+type        = string 
+} 
 
+variable "permission" { 
+type        = string 
+}
+variable "service" { 
+type        = string 
+} 
+variable "identifier" { 
+type        = string 
+} 
+variable "actions" {
+type        = list(string)
+}
