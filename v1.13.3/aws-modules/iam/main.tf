05137f91cc119c4be73d320359adf3d9d2b7c22d
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 12:03:12 +0530

    Update main.tf

--- v1.13.3/aws-modules/iam/main.tf
+++ v1.13.3/aws-modules/iam/main.tf
@@ -20,5 +20,19 @@ resource "aws_iam_role_policy" "iam_inline_policies" {
 
   name   = "${each.key}-inline-policy"
   role   = aws_iam_role.roles[each.key].name
-  policy = var.inline_policies[each.value]
+  policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [{
+    Effect   = var.permission_effect
+    Action   = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    Resource = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${module.lambda.lambda_function_names[each.key]}:*"
+  }]
+})
+
 }
+
+
