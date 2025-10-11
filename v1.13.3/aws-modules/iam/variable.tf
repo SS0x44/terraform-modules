5a07332c933021e900a845e7aecfe152b0c61cb9
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 17:24:48 +0530

    Update variable.tf

--- v1.13.3/aws-modules/iam/variable.tf
+++ v1.13.3/aws-modules/iam/variable.tf
@@ -1,28 +1,8 @@
 # variables.tf
-variable "aws_account_id" {
-  type        = string
-}
-
-variable "region" {
-  type        = string
-}
-
-variable "deploy_color" {
-  type        = string
-}
-
-variable "env_tags" {
-  type        = map(string)
-}
-
 "iam_role_names" { 
 type        = list(string) 
 } 
-
-variable "inline_policies" { 
-type        = list(string) 
-} 
-
+ 
 variable "tags" { 
 type        = map(string) 
 }
