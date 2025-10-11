efa6d683c744d58db9818e623453855daf899f6e
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 16:17:28 +0530

    Update main.tf

--- v1.13.3/aws-modules/log-group/main.tf
+++ v1.13.3/aws-modules/log-group/main.tf
@@ -1,8 +1,9 @@
 resource "aws_cloudwatch_log_group" "lambda_logs" {
-  for_each = aws_lambda_function.lambda_functions
+  for_each = var.lambda_functions
 
   name              = "/aws/lambda/${each.value.function_name}"
   retention_in_days = var.retention_in_days
   tags              = var.tags
 }
+
 
