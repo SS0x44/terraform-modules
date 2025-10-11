2679fb8bf00081753ac308ca94bd0cbc7e3bcc7d
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 17:22:35 +0530

    Update variable.tf

--- v1.13.3/aws-modules/lambda/variable.tf
+++ v1.13.3/aws-modules/lambda/variable.tf
@@ -5,35 +5,21 @@ variable "function_names" {
 variable "role_arn" {
   type        = string
 }
-
-variable "s3_key" {
- type = list(string)
-}
-
 variable "bucket_name" {
 type = string
 }
-
-variable "source" {
-type = string
-}
-
 variable "handlers" {
   type = list(string)
 }
-
 variable "runtimes" {
   type = list(string)
 }
-
 variable "timeouts" {
   type = list(number)
 }
-
 variable "memory_sizes" {
   type = list(number)
 }
-
 variable "environment_variables" {
   type = list(map(string))
 }
