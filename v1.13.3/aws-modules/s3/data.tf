9fa8855b7e722a94008b91c5ddbf6cd4cabc31c0
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 16:42:57 +0530

    Update data.tf

--- v1.13.3/aws-modules/s3/data.tf
+++ v1.13.3/aws-modules/s3/data.tf
@@ -1,10 +1,1 @@
-data "aws_iam_policy_document" "bucket_policy_document" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.resource_bucket.arn}/*"]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-  }
-}
+
