5c580d61b832df5351d19a8b2489b3563b66359c
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-11 17:20:27 +0530

    Update main.tf

--- v1.13.3/aws-modules/lambda/main.tf
+++ v1.13.3/aws-modules/lambda/main.tf
@@ -1,27 +1,31 @@
+locals {
+  functions = { for i, name in var.function_names : name => i }
+}
+
 resource "aws_s3_object" "lambda_artifact" {
-  count        = length(var.function_names) 
-  bucket       = var.bucket_name
-  key          = var.s3_key
-  source       = var.source
-  tags         = merge(var.tags,{ Function = var.function_names[count.index] }
+  for_each     = local.functions 
+  s3_bucket    = var.bucket_name
+  key          = "ingestion/${each.key}.zip"        
+  source       = "artifact/${each.key}.zip"        
+  tags         = merge(var.tags,{ Function = each.key }
 }
 
 resource "aws_lambda_function" "lambda_functions" {
-  for_each = { for i, name in var.function_names : name => i }
-
+  for_each     = local.functions 
   function_name    = each.key
   role             = var.role_arn
   handler          = var.handlers[each.value]
   runtime          = var.runtimes[each.value]
   timeout          = var.timeout[each.value]
   memory_size      = var.memory_size[each.value]
-  s3_bucket        = var.bucket_name[each.value]  
-  s3_key           = var.s3_key[each.value]  
+  s3_bucket        = var.bucket_name 
+  s3_key           = aws_s3_object.lambda_artifact[each.key].key  
   tags             = merge(var.tags, {"FunctionName" = each.key})
   environment {
     variables      = var.environment_variables[each.value] 
   }
 }
+
 
 
 
