fb963b2f589e4baca8b0aca03836d4e8399f0586
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-17 22:49:41 +0530

    Update variable.tf

--- Gitlab/variable.tf
+++ Gitlab/variable.tf
@@ -6,7 +6,15 @@ variable "gitlab_project_name" {
 type = string
 }
 
-variable "gitlab_project_descrption" {
+variable "gitlab_group_name" {
+type = string
+}
+
+variable "gitlab_user_id" {
+type        = number
+}
+
+variable "gitlab_project_desc" {
 type = string
 }
 
@@ -15,4 +23,28 @@ type = string
 }
 variable "initialize_with_readme" {
 type = boolen
+}
+
+variable "gitlab_group_access_level" {
+type        = number
+}
+variable "gitlab_group_desc" {
+type = string
+}
+
+variable "gitlab_group_access_level" {
+type = string
+}
+
+variable "project_env_var_key" {
+type = string
+}
+variable "project_env_var_value" {
+type = string
+}
+variable "env_var_is_protected" {
+type = boolean
+}
+variable "env_var_is_masked" {
+type = boolean
 }
