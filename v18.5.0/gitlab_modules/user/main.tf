1071c9260fda0a1096a57b0008bfe083d81ada2b
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-17 23:50:27 +0530

    Update main.tf

--- v18.5.0/gitlab_modules/user/main.tf
+++ v18.5.0/gitlab_modules/user/main.tf
@@ -1,1 +1,12 @@
-
+resource "gitlab_user" "group_member" {
+  count            = var.create_user ? 1 : 0
+  name             = var.gitlab_username
+  username         = var.gitlab_username
+  password         = var.gitlab_password
+  email            = var.gitlab_email
+  is_admin         = var.is_admin
+  projects_limit   = var.projects_limit
+  can_create_group = var.can_create_group
+  is_external      = var.is_external
+  reset_password   = var.reset_password
+}
