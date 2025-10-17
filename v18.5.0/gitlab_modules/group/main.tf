0b4b13243bc50189c40d611f1fa444bef097c0e9
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-17 23:51:07 +0530

    Update main.tf

--- v18.5.0/gitlab_modules/group/main.tf
+++ v18.5.0/gitlab_modules/group/main.tf
@@ -1,1 +1,14 @@
-.
+resource "gitlab_group" "gitlab_group" {
+  count            = var.create_group ? 1 : 0
+  name             = var.gitlab_group_name
+  path             = var.gitlab_group_path
+  description      = var.gitlab_group_desc
+  visibility_level = var.gitlab_group_vis_level
+}
+
+resource "gitlab_group_membership" "group_membership" {
+  count         = var.create_membership ? 1 : 0
+  group_id      = gitlab_group.gitlab_group[0].id
+  user_id       = var.create_user ? gitlab_user.group_member[0].id : var.gitlab_user_id
+  access_level  = var.gitlab_group_access_level
+}
