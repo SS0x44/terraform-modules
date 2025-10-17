e44a2db91d7493866908e3318271c676c94ac545
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2025-10-17 23:11:36 +0530

    Update output.tf

--- Gitlab/output.tf
+++ Gitlab/output.tf
@@ -1,3 +1,11 @@
-output "gitlab_project" {
-value = gitlab_project.cicd_gitlab.name
+output "gitlab_project_name" {
+  value = var.create_project ? gitlab_project.cicd_gitlab[0].name : null
+}
+
+output "gitlab_group_id" {
+  value = var.create_membership ? gitlab_group_membership.group_membership[0].group_id : null
+}
+
+output "gitlab_project_id" {
+  value = var.create_project ? gitlab_project.cicd_gitlab[0].id : null
 }
